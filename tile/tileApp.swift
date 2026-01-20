8575be39e3671300b05a569b3bad36cc7c8bbd29
tal <nexus0x42@gmail.com>
2026-01-19 14:13:13 -1000

    Initial Commit

---
+++ tile/tileApp.swift
@@ -0,0 +1,17 @@
+//
+//  tileApp.swift
+//  tile
+//
+//  Created by tal on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct tileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
